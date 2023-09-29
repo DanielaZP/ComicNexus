0cb925791907cd78b2ef094a2b04bacbc7436160
devferx <ferkki@hotmail.com>
2023-09-28 23:37:12 -0400

    Corrección de estilos

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,52 +1,107 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">ComicsNexus</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <a className="navbar-brand" href="/">
+            ComicsNexus
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to='/'>Inicio</Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link" to='/destacados'>Destacados</Link>
-                </li>
-                <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Categorias
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Inicio
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/destacados">
+                  Destacados
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Categorias
                 </a>
                 <ul class="dropdown-menu">
-                    <li><Link className="dropdown-item" to='/terror'>Terror</Link></li>
-                    <li><Link className="dropdown-item" to='/accion'>Accion</Link></li>
-                    <li><Link className="dropdown-item" to='/terror'>Ciencia ficcion</Link></li>
-                    <li><Link className="dropdown-item" to='/terror'>Ciencia ficcion</Link></li>
-                    <li><Link className="dropdown-item" to='/terror'>Ciencia ficcion</Link></li>
+                  <li>
+                    <Link className="dropdown-item" to="/terror">
+                      Terror
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/accion">
+                      Accion
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/terror">
+                      Ciencia ficcion
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/terror">
+                      Ciencia ficcion
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="/terror">
+                      Ciencia ficcion
+                    </Link>
+                  </li>
                 </ul>
-                </li>
+              </li>
             </ul>
-            {/* <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Buscar</button>
-            </form> */}
-            </div>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Buscar"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Buscar
+              </button>
+            </form>
+          </div>
         </div>
-            <div class="contenedor-icono">
-                <Link class="navbar-brand" to='/registro-comic'><i class="bi bi-person-circle"></i></Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
-        </nav>
+        <div class="contenedor-icono">
+          <Link class="navbar-brand" to="/registro-comic">
+            <i class="bi bi-person-circle"></i>
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/paginas/RegistroComic.js
+++ src/paginas/RegistroComic.js
@@ -1,57 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export const RegistroComic = () => {
-  const [fechaPublicacion, setFechaPublicacion] = useState('');
+  const [fechaPublicacion, setFechaPublicacion] = useState("");
 
   const handleFechaChange = (e) => {
     setFechaPublicacion(e.target.value);
   };
 
   return (
-<div className='container'>
-      <h1>Registrar Comic</h1>
-      <div className='row'>
-        <div className='col-6'>
-        <img src="./heroes/marvel-iron.jpg" class="img-thumbnail" style={{width: "350px"}} alt="..."/>
-        </div>
-        <div className='col-6'>
-        <form className='formRegister'>
-        <div className="form-group">
-          <label htmlFor="titulo">Título:</label>
-          <input type="text" id="titulo" name="titulo" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="autores">Autores:</label>
-          <input type="text" id="autores" name="autores" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="categorias">Categorías:</label>
-          <input type="text" id="categorias" name="categorias" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="fechaPublicacion">Año de Publicación:</label>
-          <input
-            type="date"
-            id="fechaPublicacion"
-            name="fechaPublicacion"
-            value={fechaPublicacion}
-            onChange={handleFechaChange}
+    <div className="container">
+      <h1 className="text-center mb-4">Registrar Comic</h1>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="./heroes/marvel-iron.jpg"
+            class="img-thumbnail mx-auto d-block"
+            style={{ width: "300px" }}
+            alt="..."
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="sinopsis">Sinopsis:</label>
-          <textarea id="sinopsis" name="sinopsis" rows="4" cols="50" />
-        </div>
-        <div className="button-group">
-          <button type="submit">Cancelar</button>
-          <button type="reset">Limpiar</button>
-          <button type="button">Guardar</button>
-        </div>
-      </form>
+        <div className="col-6">
+          <form className="formRegister">
+            <div className="form-group">
+              <label className="form-label" htmlFor="titulo">
+                Título
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                id="titulo"
+                name="titulo"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="autores">Autores</label>
+              <input
+                className="form-control"
+                type="text"
+                id="autores"
+                name="autores"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="categorias">Categorías</label>
+              <input
+                className="form-control"
+                type="text"
+                id="categorias"
+                name="categorias"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="fechaPublicacion">Año de Publicación</label>
+              <input
+                className="form-control"
+                type="date"
+                id="fechaPublicacion"
+                name="fechaPublicacion"
+                value={fechaPublicacion}
+                onChange={handleFechaChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="sinopsis">Sinopsis:</label>
+              <textarea
+                className="form-control"
+                id="sinopsis"
+                name="sinopsis"
+                rows="4"
+                cols="50"
+              />
+            </div>
+            <div className="d-flex justify-content-between">
+              <button type="submit">Cancelar</button>
+              <button type="reset">Limpiar</button>
+              <button type="button">Guardar</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
   );
 };
-
-

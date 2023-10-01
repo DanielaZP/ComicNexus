5aad5b386ec41fe848c4c74f9fb8410100c40970
unknown <202103159@est.umss.edu>
2023-10-01 02:15:20 -0400

    Opcion de seleccionar imagen,visualizarla y seleccionar varias categorias al momento de registrar datos de un comic

--- src/paginas/RegistroComic.jsx
+++ src/paginas/RegistroComic.jsx
@@ -1,24 +1,93 @@
 import React, { useState } from "react";
+import Select from "react-select";
 
 export const RegistroComic = () => {
   const [fechaPublicacion, setFechaPublicacion] = useState("");
+  const [imageUrl, setImageUrl] = useState(null);
+  const [hasImage, setHasImage] = useState(false);
 
   const handleFechaChange = (e) => {
     setFechaPublicacion(e.target.value);
   };
 
+  const options = [
+    { value: "Terror", label: "Terror" },
+    { value: "Accion", label: "Acción" },
+    { value: "CienciaFiccion", label: "Ciencia Ficción" },
+    { value: "Romance", label: "Romance" },
+    { value: "Comedia", label: "Comedia" },
+  ];
+  const [selectedCategorias, setSelectedCategorias] = useState([]);
+  const handleCategoriaChange = (selectedOptions) => {
+    setSelectedCategorias(selectedOptions);
+  };
+
+ 
+  const handleImageUpload = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const imageUrl = event.target.result;
+        setImageUrl(imageUrl);
+        setHasImage(true); 
+      };
+      reader.readAsDataURL(selectedFile);
+    } else {
+      setImageUrl(""); 
+      setHasImage(false); 
+    }
+  };
+
+
   return (
     <div className="container">
       <h1 className="text-center mb-4">Registrar Comic</h1>
+  
       <div className="row">
-        <div className="col-6">
-          <img
-            src="./heroes/marvel-iron.jpg"
-            className="img-thumbnail mx-auto d-block"
-            style={{ width: "300px" }}
-            alt="..."
-          />
+      <div className="col-6">
+          <div className="form-group">
+            <label htmlFor="imagen"></label>
+            <div className="image-container">
+              {hasImage ? (
+                <img
+                  src={imageUrl}
+                  style={{ width: "300px", height: "310px" }}
+                  alt="Imagen seleccionada"
+                />
+              ) : (
+                <div
+                  className="empty-image-container"
+                  style={{
+                    width: "300px",
+                    height: "310px",
+                    border: "1px dashed #ccc",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  Ingrese una imagen de tamaño:
+                </div>
+              )}
+              <button
+                onClick={() => document.getElementById("imagen").click()}
+                className="select-image-button"
+              >
+                Seleccionar Imagen
+              </button>
+            </div>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              id="imagen"
+              name="imagen"
+              style={{ display: "none" }}
+            />
+          </div>
         </div>
+        
         <div className="col-6">
           <form className="formRegister">
             <div className="form-group">
@@ -30,6 +99,7 @@ export const RegistroComic = () => {
                 type="text"
                 id="titulo"
                 name="titulo"
+                maxlength="60"
                 required
               />
             </div>
@@ -40,24 +110,26 @@ export const RegistroComic = () => {
                 type="text"
                 id="autores"
                 name="autores"
+                maxlength="100"
               />
             </div>
             <div className="form-group">
-              <label htmlFor="categorias"> Categorías<span className="text-dangerous">*</span>
-              </label>
-              <input
-                className="form-control"
-                type="text"
-                id="categorias"
-                name="categorias"
-                required
-              />
+              <label htmlFor="categorias"> Categorías<span className="text-dangerous">*</span></label>
+              <Select
+               name="categorias"
+               options={options}
+               isMulti
+               value={selectedCategorias}
+               onChange={handleCategoriaChange}
+               />
             </div>
+            
             <div className="form-group">
               <label htmlFor="fechaPublicacion">Año de Publicación</label>
               <input
                 className="form-control"
                 type="date"
+                placeholder="dd/mm/aaaa"
                 id="fechaPublicacion"
                 name="fechaPublicacion"
                 value={fechaPublicacion}
@@ -71,8 +143,7 @@ export const RegistroComic = () => {
                 className="form-control"
                 id="sinopsis"
                 name="sinopsis"
-                rows="4"
-                cols="50"
+                maxlength="500"
                 required
               />
             </div>

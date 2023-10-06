ce80aa936c0d687405d9b37499a98b5fca2a84de
MichelleBarriga <barriga.rios.michelle1@gmail.com>
2023-10-06 17:46:42 -0400

    Mensaje de ComicsXCategoria no encontrados

--- src/paginas/Accion.jsx
+++ src/paginas/Accion.jsx
@@ -34,13 +34,19 @@ useEffect(() => {
       <hr className="my-4" style={{ borderColor: 'var(--celestito)', borderWidth: '2px' }} />
     </Container> 
     <div className="container">
-      <div className="row row-cols-1 row-cols-md-4 g-4 mt-4">
-        {comicsData.map((comic) => (
-          <div className="col-md-4" key={comic.comic.cod_comic}>
-          <CardCat comic={comic} />
-        </div>
-        ))}
-      </div>
+    {isLoading ? (
+          <p>Cargando cómics...</p>
+        ) : comicsData.length === 0 ? (
+          <p>No se encontraron cómics en esta categoría.</p>
+        ) : (
+          <div className="row row-cols-1 row-cols-md-4 g-4 mt-4">
+            {comicsData.map((comic) => (
+              <div className="col-md-4" key={comic.comic.cod_comic}>
+                <CardCat comic={comic} />
+              </div>
+            ))}
+          </div>
+        )}
     </div>
   </div>
   )

2e32da960e8a01417f92ff34b1d51ffc3fc748ae
unknown <202103159@est.umss.edu>
2023-10-18 16:13:12 -0400

    Actualizacion de la interfaz Registro de Usuario

--- src/paginas/RegistroUsuario.jsx
+++ src/paginas/RegistroUsuario.jsx
@@ -1,4 +1,29 @@
 import React, { useState } from 'react';
 
+function RegistroUsuario() {
+    const [formData, setFormData] = useState({
+      username: '',
+      email: '',
+      password: '',
+    });
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      //bd
+      console.log(formData);
+    };
+  
+    return (
+      <div>
+        <h2>Registro de Usuario</h2>
+        
+      </div>
+    );
+  }
 
 export default RegistroUsuario

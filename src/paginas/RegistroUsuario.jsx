6be66266d4b0e4e106884e360af22b9bfb82e381
unknown <202103159@est.umss.edu>
2023-10-22 18:06:25 -0400

    Implementacion de campos obligatorios en Registro

--- src/paginas/RegistroUsuario.jsx
+++ src/paginas/RegistroUsuario.jsx
@@ -5,10 +5,18 @@ import { Link } from 'react-router-dom';
 
 function RegistroUsuario() {
   const [formData, setFormData] = useState({
+    name: '',
     username: '',
     email: '',
     password: '',
-    showPassword: false, 
+    showPassword: false,
+  });
+
+  const [errors, setErrors] = useState({
+    name: '',
+    username: '',
+    email: '',
+    password: '',
   });
 
   const togglePasswordVisibility = () => {
@@ -17,13 +25,33 @@ function RegistroUsuario() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    setErrors({ ...errors, [name]: '' });
     setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // bd
-    console.log(formData);
+
+    const newErrors = {};
+    if (!formData.name) {
+      newErrors.name = 'El nombre completo es obligatorio.';
+    }
+    if (!formData.username) {
+      newErrors.username = 'El nombre de usuario es obligatorio.';
+    }
+    if (!formData.email) {
+      newErrors.email = 'El correo electrónico es obligatorio.';
+    }
+    if (!formData.password) {
+      newErrors.password = 'La contraseña es obligatoria.';
+    }
+
+    if (Object.keys(newErrors).length === 0) {
+      // Lógica de registro
+      console.log('Registro exitoso con:', formData);
+    }
+
+    setErrors(newErrors);
   };
 
   return (
@@ -34,34 +62,43 @@ function RegistroUsuario() {
           <img src="./LogoComicsNexus.png" alt="Imagen de usuario" style={{ maxWidth: '50%', height: 'auto' }} />
         </div>
         <div className="form-group">
-          <label>Nombre completo:</label>
+          <label>Nombre completo<span className="text-danger">*</span></label>
           <input
             type="text"
             name="name"
             value={formData.name}
             onChange={handleChange}
           />
+          <p className={`error-message ${errors.name ? '' : 'hidden'}`}>
+            {errors.name}
+          </p>
         </div>
         <div className="form-group">
-          <label>Nombre de usuario:</label>
+          <label>Nombre de usuario<span className="text-danger">*</span></label>
           <input
             type="text"
             name="username"
             value={formData.username}
             onChange={handleChange}
           />
+          <p className={`error-message ${errors.username ? '' : 'hidden'}`}>
+            {errors.username}
+          </p>
         </div>
         <div className="form-group">
-          <label>Correo electrónico:</label>
+          <label>Correo electrónico<span className="text-danger">*</span></label>
           <input
             type="email"
             name="email"
             value={formData.email}
             onChange={handleChange}
           />
+          <p className={`error-message ${errors.email ? '' : 'hidden'}`}>
+            {errors.email}
+          </p>
         </div>
         <div className="form-group">
-          <label>Contraseña:</label>
+          <label>Contraseña<span className="text-danger">*</span></label>
           <div className="password-input">
             <input
               type={formData.showPassword ? 'text' : 'password'}
@@ -75,12 +112,15 @@ function RegistroUsuario() {
               className="password-toggle"
             />
           </div>
+          <p className={`error-message ${errors.password ? '' : 'hidden'}`}>
+            {errors.password}
+          </p>
         </div>
         <div className="form-group" style={{ textAlign: 'center' }}>
           <button type="submit">Registrarse</button>
         </div>
         <div style={{ textAlign: 'center', marginTop: '10px' }}>
-          ¿Ya tienes cuenta? <Link to="/inicio-sesion"> Inicia sesión.</Link>
+          ¿Ya tienes cuenta? <Link to="/inicio-sesion">Inicia sesión aquí.</Link>
         </div>
       </form>
     </div>

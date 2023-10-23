036d3d9b0523db7fa5f711be7f5317ca54016246
marckstar-RjJ <202001810@est.umss.edu>
2023-10-23 01:12:15 -0400

    adevretncias mejoradas plylist

--- src/paginas/Playlist.jsx
+++ src/paginas/Playlist.jsx
@@ -1,13 +1,18 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Button, Modal, Form, Container, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
+import { Spinner } from 'react-bootstrap';
 
 function Playlist() {
   const [showModal, setShowModal] = useState(false);
+  const [confirmModalVisible, setConfirmModalVisible] = useState(false);
+  const [confirmed, setConfirmed] = useState(false);
   const [successModalVisible, setSuccessModalVisible] = useState(false);
   const [playlistName, setPlaylistName] = useState('');
   const [selectedImage, setSelectedImage] = useState(null);
   const [nameError, setNameError] = useState('');
+  const [playlists, setPlaylists] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const imageUploadRef = useRef(null);
 
   const handleClose = () => {
@@ -30,13 +35,15 @@ function Playlist() {
       const allowedExtensions = /(\.png|\.jpg)$/i;
       if (!allowedExtensions.exec(file.name)) {
         window.alert('Por favor, selecciona un archivo de imagen válido (png o jpg).');
-        e.target.value = ''; // Limpiar la selección del archivo
+        e.target.value = '';
         return;
       }
 
       const reader = new FileReader();
       reader.onloadend = () => {
         setSelectedImage(reader.result);
+        // Limpiar el mensaje de error cuando se selecciona una imagen válida
+        setNameError('');
       };
       reader.readAsDataURL(file);
     } else {
@@ -44,30 +51,52 @@ function Playlist() {
     }
   };
 
+  useEffect(() => {
+    axios
+      .get('https://comic-next-laravel.vercel.app/api/api/listasplaylists')
+      .then((response) => {
+        setPlaylists(response.data);
+      })
+      .catch((error) => {
+        console.error('Error al obtener datos:', error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
   const handleSavePlaylist = () => {
     if (!playlistName.trim()) {
       setNameError('El nombre de la playlist es obligatorio.');
     } else if (!selectedImage) {
       setNameError('Sube una imagen válida.');
     } else {
-      const base64Image = extractBase64Code(selectedImage);
-
-      const data = {
-        nombre_playlist: playlistName,
-        imagen_playlist: base64Image,
-        cod_usuario: 1
-      };
-
-      axios.post('https://comic-next-laravel.vercel.app/api/api/registroplay', data)
-        .then(() => {
-          setSuccessModalVisible(true);
-          handleClose();
-        })
-        .catch((error) => {
-          window.alert('¡Error al subir la playlist!');
-          console.error('Error al enviar los datos:', error);
-        });
+      setConfirmModalVisible(true);
     }
+  };
+
+  const handleConfirmSave = () => {
+    const base64Image = extractBase64Code(selectedImage);
+    const data = {
+      nombre_playlist: playlistName,
+      imagen_playlist: base64Image,
+      cod_usuario: 1
+    };
+
+    axios
+      .post('https://comic-next-laravel.vercel.app/api/api/registroplay', data)
+      .then(() => {
+        setSuccessModalVisible(true);
+        handleClose();
+      })
+      .catch((error) => {
+        window.alert('¡Error al subir la playlist!');
+        console.error('Error al enviar los datos:', error);
+      })
+      .finally(() => {
+        setConfirmModalVisible(false);
+        setConfirmed(false);
+      });
   };
 
   const extractBase64Code = (dataURL) => {
@@ -81,7 +110,11 @@ function Playlist() {
       <hr className="my-4" style={{ borderColor: 'var(--celestito)', borderWidth: '2px' }} />
 
       <div className="container">
-        <Button variant="btn custom-btn-color" onClick={handleShow} style={{ marginTop: '-155px', marginLeft: '900px' }}>
+        <Button
+          variant="btn custom-btn-color"
+          onClick={handleShow}
+          style={{ marginTop: '-155px', marginLeft: '900px' }}
+        >
           Crear playlist
         </Button>
 
@@ -103,7 +136,7 @@ function Playlist() {
                       alignItems: 'center',
                       flexDirection: 'column',
                       marginBottom: '20px',
-                      position: 'relative',
+                      position: 'relative'
                     }}
                   >
                     {selectedImage ? (
@@ -124,7 +157,7 @@ function Playlist() {
                         position: 'absolute',
                         bottom: 0,
                         left: '50%',
-                        transform: 'translateX(-50%)',
+                        transform: 'translateX(-50%)'
                       }}
                       onChange={handleImageChange}
                     />
@@ -164,6 +197,20 @@ function Playlist() {
           </Modal.Body>
         </Modal>
 
+        <Modal show={confirmModalVisible} onHide={() => setConfirmModalVisible(false)} centered>
+          <Modal.Body>
+            <h4>¿Estás seguro de que deseas guardar esta playlist?</h4>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={() => setConfirmModalVisible(false)}>
+              No
+            </Button>
+            <Button variant="success" onClick={handleConfirmSave}>
+              Sí
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
         <Modal show={successModalVisible} onHide={() => setSuccessModalVisible(false)} centered>
           <Modal.Body>
             <h4>¡La playlist se ha subido con éxito!</h4>
@@ -174,6 +221,30 @@ function Playlist() {
             </Button>
           </Modal.Footer>
         </Modal>
+
+        <Row>
+          {isLoading ? (
+            <div className="text-center my-3">
+              <Spinner animation="border" variant="primary" role="status">
+                <span className="sr-only">.</span>
+              </Spinner>
+              <p className="mt-2">Cargando playlists...</p>
+            </div>
+          ) : playlists.length === 0 ? (
+            <p>No se encontraron playlists.</p>
+          ) : (
+            playlists.map((playlist) => (
+              <Col key={playlist.id} md={2} className="mb-4">
+                <div className="card">
+                  <img src={playlist.imagen_playlist} className="card-img-top" alt={playlist.nombre_playlist} />
+                  <div className="card-body">
+                    <h5 className="card-title">{playlist.nombre_playlist}</h5>
+                  </div>
+                </div>
+              </Col>
+            ))
+          )}
+        </Row>
       </div>
     </Container>
   );

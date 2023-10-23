638b0f0c4881a503450df312c1be4e39250cb345
rolando <rolamdomr@gmail.com>
2023-10-23 07:04:15 -0400

    Mensaje de advertencia, Desabilitar enter y redirrecionamineto

---
+++ src/paginas/BusquedaTodo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Spinner,Row,Col,Button,Modal} from 'react-bootstrap';
+function BusquedaTodo() {
+  return (
+    <Container className="text-center my-5">
+          <h1 className="display-4 badabb">Resultado</h1>
+          <hr className="my-4" style={{ borderColor: 'var(--celestito)', borderWidth: '2px' }} />
+    </Container>
+    //contenido de la pagina
+  )
+}
+
+export default BusquedaTodo;

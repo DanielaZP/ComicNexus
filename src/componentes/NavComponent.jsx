e0a21dbe10c18f4eabe6d8971f87c6b057f21727
rolando <rolamdomr@gmail.com>
2023-10-23 17:04:49 -0400

    Buscador en tiempo real

--- src/componentes/NavComponent.jsx
+++ src/componentes/NavComponent.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from 'react-router-dom'; 
 
+
 const NavComponent = () => {
   let navigate = useNavigate();
   const [inputValue, setInputValue] = useState('');
@@ -16,17 +17,22 @@ const NavComponent = () => {
 
   const hideInput = () => {
     setInputVisible(false);
+    setShowAlert(false);
     setInputValue('');
     navigate(-1);
   };
 
   const handleInputChange = (e) => {
     const value = e.target.value;
-    if(value.length === 1){navigate('/buscar');}
-    if(value.length <= 30) {
+    const otro = 'zzz'
+    if(value == 0){
       setInputValue(value);
       setShowAlert(false);
-      console.log(value)
+      navigate(`/buscar?search=${otro}`);}
+    else if(value.length <= 30) {
+      setInputValue(value);
+      setShowAlert(false);
+      navigate(`/buscar?search=${value}`);
     } else {
       setShowAlert(true);
     }
@@ -41,6 +47,8 @@ const NavComponent = () => {
       e.preventDefault(); 
     }
   }
+
+  const borrar = ()=>{setShowAlert(false);}
 
   return (
     <Navbar>
@@ -75,12 +83,13 @@ const NavComponent = () => {
                       ><i class="bi bi-trash-fill"></i></span>
                       )}{showAlert && (
                         <div class="alert alert-danger d-flex align-items-center" role="alert" 
-                             style={{position:"absolute", height:"25px", top:"35px"}}>
+                             style={{position:"absolute", height:"25px", top:"35px", width:'317px', right:'0px'}}>
                           <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" 
                               viewBox="0 0 16 16" role="img" aria-label="Warning:" style={{width: "16px", height: "16px"}}>
                             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                           </svg>
-                          <div> No puede ingresar más de 30 caracteres </div>
+                          <div> No puede ingresar más de 30 caracteres</div>
+                          <button type="button" className="btn-close" onClick={borrar} aria-label="Close" ></button>
                         </div> )}               
                 </div>
                 </form>
--- src/paginas/BusquedaTodo.jsx
+++ src/paginas/BusquedaTodo.jsx
@@ -1,12 +1,89 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Spinner,Row,Col,Button,Modal} from 'react-bootstrap';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios'; // Importa Axios
+import CardCat from '../componentes/CardCat'// Importa tu componente Card
+
 function BusquedaTodo() {
+  const [comicsData, setComicsData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Inicialmente, isLoading se establece en true
+  const itemsPerPage = 9;
+  const [currentPage, setCurrentPage] = useState(1);
+  const location = useLocation();
+  const search = new URLSearchParams(location.search).get("search");
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(search)
+    axios.get('https://comic-next-laravel.vercel.app/api/api/buscar/'+search)
+      .then((response) => {
+        // Almacena los datos JSON en el estado local
+        console.log(response.data);
+        setComicsData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error al obtener datos:', error);
+      })
+      .finally(() => {
+        // Establece isLoading en false una vez que la solicitud se ha completado (ya sea con éxito o con error)
+        setIsLoading(false);
+      });
+  }, [search]);
+
+  const getCurrentComics = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return comicsData.slice(startIndex, endIndex);
+  };
+
   return (
-    <Container className="text-center my-5">
-          <h1 className="display-4 badabb">Resultado</h1>
-          <hr className="my-4" style={{ borderColor: 'var(--celestito)', borderWidth: '2px' }} />
+    <div>
+      <Container className="text-center my-5">
+      <h1 className="display-4 badabb">Resultados</h1>
+      <p className="lead">Explora y descubre contenido increíble</p>
+      <hr className="my-4" style={{ borderColor: 'var(--celestito)', borderWidth: '2px' }} />
     </Container>
-    //contenido de la pagina
+    <div className="container">
+        {isLoading ? (
+         <div className="text-center my-3">
+         <Spinner animation="border" variant="primary" role="status">
+           <span className="sr-only">.</span>
+         </Spinner>
+         <p className="mt-2">Cargando cómics...</p>
+       </div>
+        ) : comicsData.length === 0 ? (
+          <p>No se encontraron cómics </p>
+        ) : (
+          <div>
+            <div className="row row-cols-1 row-cols-md-4 g-4 mt-4">
+              {getCurrentComics().map((comic) => (
+                <div className="col-md-4" key={comic.comic.cod_comic}>
+                  <CardCat comic={comic} />
+                </div>
+              ))}
+            </div>
+            {comicsData.length > itemsPerPage && (
+              <div className="mt-4 text-center">
+                <button
+                  className="btn custom-btn-color mx-2"
+                  onClick={() => setCurrentPage(currentPage - 1)}
+                  disabled={currentPage === 1}
+                >
+                  Página Anterior
+                </button>
+                <button
+                  className="btn custom-btn-color mx-2"
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                  disabled={currentPage * itemsPerPage >= comicsData.length}
+                >
+                  Siguiente Página
+                </button>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+  </div>
   )
 }
 

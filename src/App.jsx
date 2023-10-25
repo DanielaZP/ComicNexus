4bf70ed660e279fe89264221bda4b7a206fa637c
unknown <202103159@est.umss.edu>
2023-10-25 10:21:53 -0400

    Actualizaciones

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import SolicitudRestablecimientoContraseña from './paginas/SolicitudRestablecim
 function App() {
   const estiloFondo = {
     // backgroundImage: `url('fondos/fondo4.jpg')`,
+    backgroundColor: "#999999",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
     backgroundAttachment: "fixed",
